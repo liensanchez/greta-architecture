598bf0a078f27f47f5e61ad2124140bd8bb37e8f
liensanchez <liensilviosanchez@gmail.com>
2023-07-04 23:55:05 -0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>Made Better with Gretta</h1>
+      <p>Architects create buildings that are enjoyable to inhabit and interact with their surroundings. We take a more personal approach, focusing on homes as well as smaller commercial and community projects.</p>
+      <button>View our work</button>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <div>Gretta</div>
+      <button>Projects</button>
+      <button>Process</button>
+      <button>About</button>
+      <button>Get in touch</button>
+    </>
+  )
+}
+
+export default Navbar
