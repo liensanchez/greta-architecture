8fa434950d68e5325ef7cabcdd52dc66dec4cc47
liensanchez <liensilviosanchez@gmail.com>
2023-07-09 20:31:45 -0300

    new title starting with experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import img from '../../assets/Mystery Item A.png'
+import'./Experience.css'
 
 function Experience() {
   return (
-    <div>
-      <h2>* With over 20 years of architectural practise experience, we combine spatial, expressive, and technical skills to deliver efficient buildings of lasting value.</h2>
+    <div className='experienceContainer'>
+      <img src={img} alt=""  className='decorImg'/>
+      <h2> With over 20 years of architectural practise experience, we combine spatial, expressive, and technical skills to deliver efficient buildings of lasting value.</h2>
     </div>
   )
 }
