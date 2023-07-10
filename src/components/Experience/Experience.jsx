6a04629d4a0317055bffc18e848074556f96f06b
liensanchez <liensilviosanchez@gmail.com>
2023-07-09 23:33:50 -0300

    starting to work on showcase

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -5,8 +5,10 @@ import'./Experience.css'
 function Experience() {
   return (
     <div className='experienceContainer'>
+      <div className="textContainer">
+        <h2> With over 20 years of architectural practise experience, we combine spatial, expressive, and technical skills to deliver efficient buildings of lasting value.</h2>
+      </div>
       <img src={img} alt=""  className='decorImg'/>
-      <h2> With over 20 years of architectural practise experience, we combine spatial, expressive, and technical skills to deliver efficient buildings of lasting value.</h2>
     </div>
   )
 }
--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import img1 from '../../assets/Project A - Image 1.png'
+import img2 from '../../assets/Project A - Image 3.png'
+import img3 from '../../assets/Project A - Image 6.png'
+import img4 from '../../assets/Project A - Image 10.png'
+
 
 function Showcase() {
   return (
     <div>
-      <li>Dining</li>
-      <li>Office</li>
-      <li>Lounge</li>
-      <li>Bathrooms</li>
+      <div>
+        <img src={img1} alt="" />
+        <h2>Outside View</h2>
+      </div>
+      <div>
+        <img src={img2} alt="" />
+        <h2>Living Room</h2>
+      </div>
+      <div>
+        <img src={img4} alt="" />
+        <h2>Kitchen</h2>
+      </div>
+      <div>
+        <img src={img3} alt="" />
+        <h2>Bathroom</h2>
+      </div>
     </div>
   )
 }
